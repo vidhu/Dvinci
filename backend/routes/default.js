33c114b3d1c2b6a964f1409cfcf786d89b51f68b
Shresth Bhatnagar <shresth.bhatnagar@bitsighttech.com>
2018-04-14 14:25:28 -0700

    Added mocked analysis pai

--- backend/routes/default.js
+++ backend/routes/default.js
@@ -3,17 +3,31 @@ let router = express.Router();
 let Google = require('../lib/google');
 
 // define the about route
-router.get('/geocode', (req, res) => {
+router.get('/roof', (req, res) => {
     console.log(req.query);
-    if(!('ad' in req.query)){
+    if (!('ad' in req.query)) {
         res.send("Error");
         return;
     }
-    
+
     Google.geocode(req.query.ad)
-    .then((geocode) => {
-        res.send(geocode.geometry.location);
-    })
+        .then((geocode) => {
+            let solarInfo = {
+                ...geocode.geometry.location,
+                solarArea: '832',
+                units: 'sqf'
+            };
+            res.send(solarInfo);
+        });
+});
+
+router.get('/analyze', (req, res) => {
+    let analysis = {
+        currentBillAmount: 150,
+        kilowattHr: 750,
+    };
+    analysis['rate'] = analysis.currentBillAmount / analysis.kilowattHr;
+    res.send(analysis);
 });
 
 module.exports = router;
